73a75528cd09ca144fa098db7c47860ab7971110
Pablo Sánchez Alonso <pablo.sanchez.alonso@gmail.com>
2021-05-09 11:02:25 +0100

    Implemented cancel function

--- src/vip_8/events.clj
+++ src/vip_8/events.clj
@@ -1,9 +1,14 @@
 (ns vip-8.events)
 
 (def active-mode (atom '(:pause)))
+(def cancel-mode (atom '()))
 
 (defn mode 
   ([] @active-mode)
-  ([m] (swap! active-mode (fn [_] m))))
+  ([m] 
+   (when (not= (first (mode)) :command)
+     (swap! cancel-mode (fn [_] (mode))))
+   (swap! active-mode (fn [_] m))))
 
-(defn cancel [])
+(defn cancel []
+  (mode @cancel-mode))
--- test/vip_8/events_test.clj
+++ test/vip_8/events_test.clj
@@ -3,6 +3,7 @@
             [vip-8.events :refer :all]))
 
 (deftest mode-test
+  (mode '(:pause)) ;; Make sure test don't affect each other
   (testing "default mode is pause"
     (is (= (mode) '(:pause))))
   (testing "set mode to load test ROM"
@@ -15,3 +16,26 @@
     (mode '(:run))
     (is (= (mode) '(:run)))))
 
+(deftest cancel-test
+  (testing "if it was paused it comes back to paused"
+    (mode '(:pause))
+    (mode '(:command ":"))
+    
+    (cancel)
+    
+    (is (= (mode) '(:pause))))
+  (testing "if it was running, it comes back to running"
+    (mode '(:run "chipquarium.ch8"))
+    (mode '(:command ":"))
+    
+    (cancel)
+    
+    (is (= (mode) '(:run "chipquarium.ch8"))))
+  (testing "cancel always leaves command mode"
+    (mode '(:pause "tetris.ch8"))
+    (mode '(:command ":"))
+    (mode '(:command ":l"))
+    
+    (cancel)
+    
+    (is (= (mode) '(:pause "tetris.ch8")))))
